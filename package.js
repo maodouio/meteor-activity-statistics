303892fccd78241a28122c16c0deaa7377ea0ad9
mycode <2894220@qq.com>
2015-12-03 15:35:00 +0800

    Update version

--- package.js
+++ package.js
@@ -2,9 +2,9 @@ Package.describe({
   name: 'maodouio:activity-statistics',
   version: '0.0.1',
   // Brief, one-line summary of the package.
-  summary: '',
+  summary: 'Maodou.io activity-statistics package.',
   // URL to the Git repository containing the source code for this package.
-  git: 'https://github.com/maodouio/meteor-activity-statistics.git',
+  git: 'https://github.com/maodouio/meteor-activity-statistics',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
